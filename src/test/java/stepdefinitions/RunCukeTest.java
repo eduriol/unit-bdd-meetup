db73799b2269675562d8c1a19133f2a2ee21b735
Eduardo Riol Fernández <eriol@atsistemas.com>
2018-03-05 17:21:05 +0100

    Añadida opción para generación de report en json

--- src/test/java/stepdefinitions/RunCukeTest.java
+++ src/test/java/stepdefinitions/RunCukeTest.java
@@ -7,7 +7,8 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-	features = "src/test/resources"
+	features = "src/test/resources",
+	plugin = "json:target/cucumber-json-report.json"
 )
 
 public class RunCukeTest {
